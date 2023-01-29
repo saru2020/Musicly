d5823b48a172f95a173340525678dc40b827921c
Saravanan <saru2020@gmail.com>
2023-01-29 21:39:10 +0530

    RN upgrade: iOS fixes
    details:
    - [x] Issue: Warning: “rctbridge required dispatch_sync to load rctdevsettings. this may lead to deadlocks” ; Solution: [refer: https://amanhimself.dev/blog/rctbridge-required-dispatch-sync-to-load-warning/]
    - [x] Xcodeproj upgrade in React Native 0.60.0 [refer: https://github.com/react-native-community/upgrade-support/issues/14] —> removed the linked libraries since these dependencies were now being managed by cocoa pods itself
    - [x] Use of undeclared identifier FlipperClient [refer: https://github.com/react-native-community/upgrade-support/issues/24#issuecomment-606631103]
    - [x] Issue: “atomic_notify_one<unsigned%20long>” ; Solution: [refer: https://stackoverflow.com/a/70874670/1374730]

--- ios/Musicly/AppDelegate.m
+++ ios/Musicly/AppDelegate.m
@@ -1,5 +1,10 @@
 #import "AppDelegate.h"
 
+// Add this
+#if RCT_DEV
+#import <React/RCTDevLoadingView.h>
+#endif
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -32,6 +37,11 @@ static void InitializeFlipper(UIApplication *application) {
   #endif
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  
+  #if RCT_DEV
+    [bridge moduleForClass:[RCTDevLoadingView class]];
+  #endif
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"Musicly"
                                             initialProperties:nil];
